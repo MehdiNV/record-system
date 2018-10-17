b12fe184e0121d14f63d30bb6e8e60c63db09537
Mehdi <35583819+MehdiNV@users.noreply.github.com>
2018-10-17 21:48:47 +0100

    Add files via upload

---
+++ Code classes/TypeOfStudent.java
@@ -0,0 +1,11 @@
+package main;
+
+//Enum for all possible types of students that this system can have - UG,PGT,PGR
+//In the future however, other values can also be added for other types of students if needed
+
+//Author: Mehdi Naderi Varandi
+//Student ID: 170725695
+
+public enum TypeOfStudent {
+	UG,PGT,PGR
+}
