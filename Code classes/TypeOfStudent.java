420e1c585a30b3ee2ff65d3d0a76f8cd9144e9e5
Mehdi <35583819+MehdiNV@users.noreply.github.com>
2018-10-17 21:49:40 +0100

    Update TypeOfStudent.java

--- Code classes/TypeOfStudent.java
+++ Code classes/TypeOfStudent.java
@@ -4,7 +4,7 @@ package main;
 //In the future however, other values can also be added for other types of students if needed
 
 //Author: Mehdi Naderi Varandi
-//Student ID: 170725695
+
 
 public enum TypeOfStudent {
 	UG,PGT,PGR
