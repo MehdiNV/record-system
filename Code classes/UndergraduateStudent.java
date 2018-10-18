110b28fedd34ed296b07811decb5c51004e863c8
Mehdi <35583819+MehdiNV@users.noreply.github.com>
2018-10-18 17:42:26 +0100

    Add files via upload

---
+++ Code classes/UndergraduateStudent.java
@@ -0,0 +1,23 @@
+package main;
+
+//Implementation of the TaughtStudent abstract class- stores the constructor (which calls the super constructor)...
+//...and a method that returns the type of student
+
+//Author: Mehdi Naderi Varandi
+//Student ID: 170725695
+
+public final class UndergraduateStudent extends TaughtStudent{
+	private final static TypeOfStudent TYPE=TypeOfStudent.UG;
+
+	public UndergraduateStudent(Name name,String birthday) throws IllegalArgumentException{
+		super(name,birthday);	
+	}
+	
+	
+	@Override
+	public final TypeOfStudent getType() {
+		// Returns the type of student (e.g. this student is a Undergraduate)
+		return TYPE;
+	}
+
+}
