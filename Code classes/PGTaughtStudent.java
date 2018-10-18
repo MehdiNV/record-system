ef53a2c0553db0ef5bf7a3e705c5d644abf45cda
Mehdi <35583819+MehdiNV@users.noreply.github.com>
2018-10-18 21:36:49 +0100

    Add files via upload

---
+++ Code classes/PGTaughtStudent.java
@@ -0,0 +1,22 @@
+package main;
+
+//Implementation/subclass of abstract class TaughtStudent - stores the constructor (which calls the super constructor)...
+//...and a method that returns the type of student
+
+//Author: Mehdi Naderi Varandi
+//Student ID: 170725695
+
+public final class PGTaughtStudent extends TaughtStudent{
+	private final static TypeOfStudent TYPE=TypeOfStudent.PGT;
+	//Stores what type of student this class is 
+	
+	public PGTaughtStudent(Name name, String birthday) throws IllegalArgumentException {
+		super(name, birthday);
+	}
+
+	@Override
+	public final TypeOfStudent getType() {
+		return TYPE;
+	}
+
+}
