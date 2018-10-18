d9ea90e72a1e5fdadbe137d4849a60e5d9249db8
Mehdi <35583819+MehdiNV@users.noreply.github.com>
2018-10-18 17:20:42 +0100

    Add files via upload

---
+++ Code classes/Supervisor.java
@@ -0,0 +1,41 @@
+package main;
+
+//What does this class do?
+//
+//---> This class allows you to hold onto the names of the supervisor i.e. use it as an object
+
+//Author: Mehdi Naderi Varandi
+//Student ID: 170725695
+
+public class Supervisor {
+	//FIELDS-----------------------
+	private final String name; //Name of the supervisor
+	//Once initialised, the name of the supervisor shouldn't change - to ensure that defensive programming is always followed, I usually just...
+	//..re-create the required objects if their details need to be changed instead to preserve immutability
+	
+	private static final String FILE_DELIMITER=" "; //Used for splitting the name up
+	//-----------------------------
+		
+	private Supervisor (String name){ //Constructor for the supervisor class
+		this.name=name;
+	}
+	
+	public final String getName() {
+		return name;
+	}
+	
+	public final static Supervisor getInstance(String supervisorLine){
+		String[] fileParts=supervisorLine.split(FILE_DELIMITER);
+		
+		String firstName=fileParts[0].trim();
+		String surnameName=fileParts[1].trim();
+
+		return new Supervisor((firstName + " " + surnameName));
+	}
+	
+	@Override
+	public final String toString() {
+		return getName();
+	}
+	
+}
