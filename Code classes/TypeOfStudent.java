15a2b1be2978d7ba757f69bacfb73f0d007f4a59
Mehdi Naderi Varandi <MehdiNV@hotmail.com>
2018-11-01 23:33:30 +0000

    Update TypeOfStudent.java

--- Code classes/TypeOfStudent.java
+++ Code classes/TypeOfStudent.java
@@ -1,7 +1,7 @@
 package main;
 
 //Enum for all possible types of students that this system can have - UG,PGT,PGR
-//In the future however, other values can also be added for other types of students if needed
+//In the future however, other values can also be added for other types of students if needed e.g. PhD level students
 
 //Author: Mehdi Naderi Varandi
 
